a1934f711afacc78abbd3531ccf1520df7ae74f8
Angie <angybece12@gmail.com>
2024-05-16 16:18:10 -0600

    Exercise-2.1-2.3

--- part-1/introduction-to-react/src/main.jsx
+++ part-1/introduction-to-react/src/main.jsx
@@ -1,6 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render( <App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App
+    course={{
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+      ],
+    }}
+  />
+);
