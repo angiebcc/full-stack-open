89382880d7b80f3c3d8d730c4d548a2e743c4dbd
Angie <angybece12@gmail.com>
2024-07-29 19:59:24 -0600

    Exercise-2.20

--- part-2/countries/part1/src/App.jsx
+++ part-2/countries/part1/src/App.jsx
@@ -23,6 +23,7 @@ const CountryList = ({ countries }) => {
   if (countries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
   }
+
   return (
     <ul>
       {countries.map((country) => (
@@ -32,7 +33,27 @@ const CountryList = ({ countries }) => {
   );
 };
 
-const CountryListItem = ({ country }) => {
+const CountryListItem = ({ country, fetchWeather, city }) => {
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    const fetchWeather = (city) => {
+      const api_key = import.meta.env.VITE_SOME_KEY;
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+      axios
+        .get(url)
+        .then((response) => {
+          setWeather(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
+        });
+    };
+
+    fetchWeather(country.capital);
+  }, [country.name.common]);
+
   return (
     <div>
       <h1> {country.name.common} </h1>
@@ -50,6 +71,17 @@ const CountryListItem = ({ country }) => {
         <p>No language data available</p>
       )}
       <img src={country.flags.png} alt="flag" height="200" width="250" />{" "}
+      {weather && (
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <p>Temperature: {(weather.main.temp - 273.15).toFixed(2)}°C</p>
+          <img
+            alt="weather icon"
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          />
+          <p>Wind: {weather.wind.speed} m/s</p>
+        </div>
+      )}
     </div>
   );
 };
